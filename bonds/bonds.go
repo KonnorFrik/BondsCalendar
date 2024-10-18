21eda9d7fb95a0b0f378c5ba2767bd15b3e1667a
Konnor <konnor.frik666@gmail.com>
2024-10-18 23:20:07 +0500

    apply style

--- bonds/bonds.go
+++ bonds/bonds.go
@@ -1,80 +1,79 @@
 package bonds
 
 import (
-    "time"
+	"time"
 )
 
 /* Struct for describe one bonds */
 type BondsData struct {
-    Name string
-    CouponCount int
-    CouponPeriod time.Duration
-    CouponNearPayDate time.Time
-    PayDates []time.Time
+	Name              string
+	CouponCount       int
+	CouponPeriod      time.Duration
+	CouponNearPayDate time.Time
+	PayDates          []time.Time
 }
 
 /* Struct for store multiply bonds */
 type Bonds struct {
-    Bonds []*BondsData
+	Bonds []*BondsData
 }
 
 var (
-    DefaultLocation, _ = time.LoadLocation("Asia/Yekaterinburg")
+	DefaultLocation, _ = time.LoadLocation("Asia/Yekaterinburg")
 )
 
 func BondsNew() *Bonds {
-    obj := new(Bonds)
-    obj.Bonds = make([]*BondsData, 0)
+	obj := new(Bonds)
+	obj.Bonds = make([]*BondsData, 0)
 
-    return obj
+	return obj
 }
 
 /* Append new bonds, also call CalcCouponDates before append */
 func (self *Bonds) Append(obj *BondsData) {
-    obj.CalcCouponDates()
-    self.Bonds = append(self.Bonds, obj)
+	obj.CalcCouponDates()
+	self.Bonds = append(self.Bonds, obj)
 }
 
 /* Count a payments by given year and month */
 func (self *Bonds) PayCountByYearMonth(year, month int) int {
-    var result int
-    validMonth := time.Month(month)
+	var result int
+	validMonth := time.Month(month)
 
-    for _, obj := range self.Bonds {
-        for _, date := range obj.PayDates {
-            if date.Year() == year && date.Month() == validMonth {
-                result++
-            }
-        }
-    }
+	for _, obj := range self.Bonds {
+		for _, date := range obj.PayDates {
+			if date.Year() == year && date.Month() == validMonth {
+				result++
+			}
+		}
+	}
 
-    return result
+	return result
 }
 
-func  BondsDataNew() *BondsData {
-    obj := new(BondsData)
-    obj.PayDates = make([]time.Time, 0)
-    return obj
+func BondsDataNew() *BondsData {
+	obj := new(BondsData)
+	obj.PayDates = make([]time.Time, 0)
+	return obj
 }
 
 func (self *BondsData) CalcCouponDates() {
-    self.PayDates = make([]time.Time, 0)
-    tmp := self.CouponNearPayDate
+	self.PayDates = make([]time.Time, 0)
+	tmp := self.CouponNearPayDate
 
-    for i := 0; i < self.CouponCount; i++ {
-        self.PayDates = append(self.PayDates, tmp)
-        tmp = tmp.Add(self.CouponPeriod)
-    }
+	for i := 0; i < self.CouponCount; i++ {
+		self.PayDates = append(self.PayDates, tmp)
+		tmp = tmp.Add(self.CouponPeriod)
+	}
 }
 
 /** Create a pay day date with only valid year, month and day */
 func CouponPayDay(year, month, day int) time.Time {
-    obj := time.Date(year, time.Month(month), day, 0, 0, 0, 0, DefaultLocation)
-    return obj
+	obj := time.Date(year, time.Month(month), day, 0, 0, 0, 0, DefaultLocation)
+	return obj
 }
 
 /* Create a payment duration by 2 pay dates */
 func CouponPeriodCreate(nearest, next time.Time) time.Duration {
-    return next.Sub(nearest)
+	return next.Sub(nearest)
 }
-
--- utils.go
+++ utils.go
@@ -1,78 +1,78 @@
 package main
 
 import (
-    "strconv"
-    "time"
+	"strconv"
+	"time"
 
 	"github.com/gbin/goncurses"
 )
 
 /* Show message, ask input string and convert it to int */
 func AskInt(win *goncurses.Window, y, x int, msg string, maxInputLen int) (int, error) {
-    win.MovePrint(y, x, msg)
-    win.Refresh()
-    resultStr, err := win.GetString(maxInputLen)
+	win.MovePrint(y, x, msg)
+	win.Refresh()
+	resultStr, err := win.GetString(maxInputLen)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    result, err := strconv.Atoi(resultStr)
+	result, err := strconv.Atoi(resultStr)
 
-    if err != nil {
-        return 0, err
-    }
+	if err != nil {
+		return 0, err
+	}
 
-    return result, nil
+	return result, nil
 }
 
 /* Show message and ask for input string */
 func AskString(win *goncurses.Window, y, x int, msg string, maxInputLen int) (string, error) {
-    win.MovePrint(y, x, msg)
-    win.Refresh()
+	win.MovePrint(y, x, msg)
+	win.Refresh()
 
-    result, err := win.GetString(maxInputLen)
+	result, err := win.GetString(maxInputLen)
 
-    if err != nil {
-        return "", err
-    }
+	if err != nil {
+		return "", err
+	}
 
-    return result, nil
+	return result, nil
 }
 
 /* Show message, ask input string and convert it to time.Time */
 func AskDate(win *goncurses.Window, y, x int, msg string, maxInputLen int, layout string) (time.Time, error) {
-    win.MovePrint(y, x, msg)
-    win.Refresh()
+	win.MovePrint(y, x, msg)
+	win.Refresh()
 
-    resultStr, err := win.GetString(maxInputLen)
+	resultStr, err := win.GetString(maxInputLen)
 
-    if err != nil {
-        return time.Time{}, err
-    }
+	if err != nil {
+		return time.Time{}, err
+	}
 
-    result, err := time.Parse(layout, resultStr)
+	result, err := time.Parse(layout, resultStr)
 
-    if err != nil {
-        return time.Time{}, err
-    }
+	if err != nil {
+		return time.Time{}, err
+	}
 
-    return result, nil
+	return result, nil
 }
 
 /* Show one line text at new boxed window, until any input given and return last input */
 func PopUpText(y, x int, msg string) goncurses.Key {
-    winHeight, winWidth := 3, len(msg) + 2
-    win, err := goncurses.NewWindow(winHeight, winWidth, y, x)
+	winHeight, winWidth := 3, len(msg)+2
+	win, err := goncurses.NewWindow(winHeight, winWidth, y, x)
 
-    if err != nil {
-        return goncurses.Key(-1)
-    }
+	if err != nil {
+		return goncurses.Key(-1)
+	}
 
-    defer win.Delete()
-    win.Box(0, 0)
-    win.MovePrint(1, 1, msg)
-    win.Refresh()
-    input := win.GetChar()
-    return input
+	defer win.Delete()
+	win.Box(0, 0)
+	win.MovePrint(1, 1, msg)
+	win.Refresh()
+	input := win.GetChar()
+	return input
 }
