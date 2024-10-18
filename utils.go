b6e05bce877349fd0a621164b153e3bb2b50487d
Konnor <konnor.frik666@gmail.com>
2024-10-19 00:03:03 +0500

    add pop up windows for asking input

--- utils.go
+++ utils.go
@@ -76,3 +76,31 @@ func PopUpText(y, x int, msg string) goncurses.Key {
 	input := win.GetChar()
 	return input
 }
+
+/*
+Create new window at y,x.
+Show one line message and ask for input
+New window height is always 3, width len(msg) + maxInputLen + 2(for box)
+*/
+func PopUpAskString(y, x int, msg string, maxInputLen int) (string, error) {
+    winHeight, winWidth := 3, len(msg) + maxInputLen + 2
+    win, err := goncurses.NewWindow(winHeight, winWidth, y, x)
+
+    if err != nil {
+        return "", err
+    }
+
+    defer win.Delete()
+    win.Box(0, 0)
+	win.MovePrint(1, 1, msg)
+	win.Refresh()
+
+	result, err := win.GetString(maxInputLen)
+
+	if err != nil {
+		return "", err
+	}
+
+	return result, nil
+}
+
