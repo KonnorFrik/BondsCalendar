ca92e3b91f511aefa67e064c9537ad66f09a0a42
Konnor <konnor.frik666@gmail.com>
2024-10-21 00:21:56 +0500

    add function for remove expired dated for bond; change type of coupon period to int, which means days

--- bonds/bonds.go
+++ bonds/bonds.go
@@ -8,11 +8,11 @@ import (
 
 /* Struct for describe one bonds */
 type BondsData struct {
-	Name              string        `json:"name"`
-	CouponCount       int           `json:"couponCount"`
-	CouponPeriod      time.Duration `json:"couponPeriod"`
-	CouponNearPayDate time.Time     `json:"nearPayDate"`
-	PayDates          []time.Time   `json:"-"`
+	Name              string        `json:"name"` // Bond name
+	CouponCount       int           `json:"couponCount"` // Count of remaining coupon payments
+	CouponPeriod      int           `json:"couponPeriod"` // Period between coupon payments (Calc as NextDate - NearDate)
+	CouponNearPayDate time.Time     `json:"nearPayDate"` // Near date of payment
+	PayDates          []time.Time   `json:"-"` // Calculated dates of coupon payments
 }
 
 /* Struct for store multiply bonds */
@@ -73,7 +73,7 @@ func (self *Bonds) LoadFromFile(filename string) error {
 	}
 
 	for _, obj := range self.Bonds {
-		obj.CalcCouponDates()
+		obj.CalcAll()
 	}
 
 	return nil
@@ -81,7 +81,7 @@ func (self *Bonds) LoadFromFile(filename string) error {
 
 /* Append new bonds, also call CalcCouponDates before append */
 func (self *Bonds) Append(obj *BondsData) {
-	obj.CalcCouponDates()
+	obj.CalcAll()
 	self.Bonds = append(self.Bonds, obj)
 }
 
@@ -101,29 +101,63 @@ func (self *Bonds) PayCountByYearMonth(year, month int) int {
 	return result
 }
 
+/*
+Check is any bond has been expired:
+    - Near pay date is in the past and coupon count is 1
+*/
+// func (self *Bonds) ExpiredList() []int {
+//
+// }
+
 func BondsDataNew() *BondsData {
 	obj := new(BondsData)
 	obj.PayDates = make([]time.Time, 0)
 	return obj
 }
 
-func (self *BondsData) CalcCouponDates() {
-	self.PayDates = make([]time.Time, 0)
-	tmp := self.CouponNearPayDate
-
-	for i := 0; i < self.CouponCount; i++ {
-		self.PayDates = append(self.PayDates, tmp)
-		tmp = tmp.Add(self.CouponPeriod)
-	}
-}
-
-/** Create a pay day date with only valid year, month and day */
+/** Create a pay day date with only year, month and day */
 func CouponPayDay(year, month, day int) time.Time {
 	obj := time.Date(year, time.Month(month), day, 0, 0, 0, 0, DefaultLocation)
 	return obj
 }
 
-/* Create a payment duration by 2 pay dates */
-func CouponPeriodCreate(nearest, next time.Time) time.Duration {
-	return next.Sub(nearest)
+/* Create a payment duration with 2 next pay dates (near and next one) */
+func CouponPeriodCreate(nearest, next time.Time) int {
+	return int(next.Sub(nearest).Hours()) / 24
 }
+
+/*
+Caclulate all related data:
+    - Next coupon pay dates
+    - Remove dates if they are in the past (date < time.Now)
+*/
+func (self *BondsData) CalcAll() {
+    self.calcCouponDates()
+    self.removePastDates()
+}
+
+/* Check is near pay date is in the past and set near as next, if next available */
+func (self *BondsData) removePastDates() {
+    timeNow := time.Now()
+
+    for id, val := range self.PayDates {
+        if val.After(timeNow) {
+            self.PayDates = self.PayDates[id:]
+            self.CouponNearPayDate = self.PayDates[0]
+            self.CouponCount -= id
+            break
+        }
+    }
+}
+
+/* Calculate all next pay dates */
+func (self *BondsData) calcCouponDates() {
+	self.PayDates = make([]time.Time, 0)
+	tmp := self.CouponNearPayDate
+
+	for i := 0; i < self.CouponCount; i++ {
+		self.PayDates = append(self.PayDates, tmp)
+		tmp = tmp.AddDate(0, 0, self.CouponPeriod)
+	}
+}
+
